7e49316caac399d6afef25e8f2e756047724f7c6
Noah Gomes <noahgomes02@yahoo.com>
2025-01-17 01:55:41 +0000

    Revert "Update database.py"
    This reverts commit 4b9d6bfa6e28ee651074bcf4b1a5a4538ddad743.

--- project/database.py
+++ project/database.py
@@ -2,37 +2,23 @@ import os
 
 from django.conf import settings
 
+
 engines = {
     'sqlite': 'django.db.backends.sqlite3',
     'postgresql': 'django.db.backends.postgresql_psycopg2',
     'mysql': 'django.db.backends.mysql',
 }
 
-
+# test
 def config():
     service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    test_mode = os.getenv('TEST_MODE', 'false').lower() == 'true'
-
-    if test_mode:
-        # Force PostgreSQL for tests
-        return {
-            'ENGINE': engines['postgresql'],
-            'NAME': os.getenv('DATABASE_TEST_NAME', 'test_database'),
-            'USER': os.getenv('DATABASE_USER', 'postgres'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-            'HOST': os.getenv('DATABASE_HOST', 'localhost'),
-            'PORT': os.getenv('DATABASE_PORT', '5432'),
-        }
-
     if service_name:
         engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
     else:
         engine = engines['sqlite']
-
     name = os.getenv('DATABASE_NAME')
     if not name and engine == engines['sqlite']:
         name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
-
     return {
         'ENGINE': engine,
         'NAME': name,
